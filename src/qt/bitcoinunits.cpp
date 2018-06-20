1a6b6872e342694f557f19b90beec2a8f549123b
liangmusing <40384362+liangmusing@users.noreply.github.com>
2018-06-20 23:59:26 +0800

    rename lites to lamb, photons to wool

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -41,8 +41,8 @@ QString BitcoinUnits::name(int unit)
     switch(unit)
     {
     case BTC: return QString("SHC");
-    case mBTC: return QString("lites");
-    case uBTC: return QString("photons");
+    case mBTC: return QString("lambs");
+    case uBTC: return QString("wool");
     default: return QString("???");
     }
 }
